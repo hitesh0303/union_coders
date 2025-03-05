343475dac951a498d000e7ab9a4bd66c149be6da
iatharva6119 <datharva919@gmail.com>
2025-03-05 11:51:30 +0530

    Second

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional, List
+
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
